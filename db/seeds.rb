f05382a6d08349764ab6ea05946f41ab7ba4f7aa
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-04-30 23:46:57 +0200

    modification seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'faker'
+require 'faker'
 
-# Doctor.create(first_name: "andrew", last_name: "manson", specialty: "medical ethics", zip_code: "94203")
-# Doctor.create(first_name: "jack", last_name: "stapleton", specialty: "pandemics", zip_code: "72201")
-# puts "Deux docteurs ont été créés"
+Doctor.create(first_name: "andrew", last_name: "manson", specialty: "medical ethics", zip_code: "94203")
+Doctor.create(first_name: "jack", last_name: "stapleton", specialty: "pandemics", zip_code: "72201")
+puts "Deux docteurs ont été créés"
 
-# Patient.create(first_name: "jean", last_name: "dupont")
-# Patient.create(first_name: "paul", last_name: "durand")
-# puts "Deux patients ont été créés"
+Patient.create(first_name: "jean", last_name: "dupont")
+Patient.create(first_name: "paul", last_name: "durand")
+puts "Deux patients ont été créés"
 
-# 100.times do
-#     doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-#   end
-# puts "Cent nouveaux docteurs ont été générés"
+100.times do
+    doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  end
+puts "Cent nouveaux docteurs ont été générés"
 
-# 100.times do
-#     patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-#   end
-# puts "Cent nouveaux patients ont été générés"
+100.times do
+    patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  end
+puts "Cent nouveaux patients ont été générés"
 
-# City.create(city_name: "Los Angeles")
-# City.create(city_name: "New York")
-# puts "Deux villes ont été créées"
+City.create(city_name: "Los Angeles")
+City.create(city_name: "New York")
+puts "Deux villes ont été créées"
 
-# 100.times do |index|
-#     Specialty.create(specialty_name: "Specialty#{index}")
-#   end
+100.times do |index|
+    Specialty.create(specialty_name: "Specialty#{index}")
+  end
+puts "Cent spécialités ont été générées"
